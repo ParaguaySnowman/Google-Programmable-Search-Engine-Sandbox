2a2b0ec4b49d1afc2da51359dbc8b5004681989f
Parker Mortensen <parker.b.mortensen@gmail.com>
2024-12-14 09:21:18 -0700

    removing unnecessary cors dependency

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 app.use(express.static("."));
